a477e9261cfd05c03647ce036cf719d32039ce67
Peter Hollows <peter.hollows@gmail.com>
2009-12-10 16:10:53 +1100

    Changed setting of default cache paths for reuse by other classes.

--- static_caching_extension.rb
+++ static_caching_extension.rb
@@ -5,16 +5,17 @@ class StaticCachingExtension < Radiant::Extension
   version "0.8.0"
   description "Caches GET requests to disk, and purges on POST/PUT/DELETEs"
   url "http://github.com/tricycle/radiant-static-cache-extension"
-  
+
+  STATIC_CACHE_DIR = "#{RAILS_ROOT}/public/radiant-cache" unless defined?(STATIC_CACHE_DIR)
+
   def activate
     gem "tricycle-rack-contrib"
     require "rack/contrib/response_cache"
-    static_cache_dir = defined?(STATIC_CACHE_DIR) ? STATIC_CACHE_DIR : "#{RAILS_ROOT}/public/radiant-cache"
-  
-    ActionController::Dispatcher.middleware.use Rack::ResponseCache, static_cache_dir
-    ActionController::Dispatcher.middleware.use Rack::ResponseCacheSweeper, static_cache_dir
+
+    ActionController::Dispatcher.middleware.use Rack::ResponseCache, STATIC_CACHE_DIR
+    ActionController::Dispatcher.middleware.use Rack::ResponseCacheSweeper, STATIC_CACHE_DIR
   end
-  
+
   def deactivate
     ActionController::Dispatcher.middleware.delete Rack::ResponseCache
     ActionController::Dispatcher.middleware.delete Rack::ResponseCacheSweeper
